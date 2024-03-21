0fca9dcccf23575016787fa82e8c7f9e89c84136
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2024-03-21 20:03:46 +0000

    finished module-01

--- Module-01/main.py
+++ Module-01/main.py
@@ -1,1 +1,13 @@
 print("Hello, World!")
+
+PI = 3.14159
+print(PI)
+
+name = input('Enter your name:')
+print(name)
+
+x = input('Enter a number: ')
+print(type(x))
+
+x = 5
+print('The number is ' + str(x))
